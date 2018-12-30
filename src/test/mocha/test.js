97dc88b41a8fdf16a6ef0a0d5ef56c3d2b1f6356
Duncan Paterson <d.paterson@me.com>
2018-12-30 17:35:46 +0100

    fix(css): add transient css dependencies
    refactor resources directory

--- src/test/mocha/test.js
+++ src/test/mocha/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+// this is a dummy test js tests are wip
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
