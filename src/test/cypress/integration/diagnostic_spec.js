ec76adf7d3caf66a0fafe48fe10903da9b0d3ea7
duncdrum <d.paterson@me.com>
2019-05-16 16:53:04 +0200

    fix(ci): add int test to travis, add docs, remove kitchensink

---
+++ src/test/cypress/integration/diagnostic_spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+// This is very slow and therefore skipped. Once diagnostics are re-written
+// we might bring this back. In the meantime dead-links are tested via xQsuite
+// see #
+
+context.skip('Diagnostics', () => {
+  before(() => {
+    cy.visit('/diagnostics.html', {responseTimeout: 60000})
+
+  })
+  it('should not find dead links', () => {
+    cy.get('h1')
+      .contains('Documentation link diagnostics')
+      .parents('#main')
+      .find('ul')
+      .contains('advanced-installation.xml')
+  })
+})
