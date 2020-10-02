7373b9427519c9d31867eef304ee635fe5dc7c26
Duncan Paterson <d.paterson@me.com>
2020-10-02 13:22:36 +0200

    test(js): increase timeout, better description of self-test

--- src/test/mocha/test.js
+++ src/test/mocha/test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 
 // this is a dummy test js tests are wip
-describe('Array', function () {
+describe('Mocha Self-test via Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.strictEqual([1, 2, 3].indexOf(4), -1)
